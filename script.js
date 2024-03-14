615245e88b96ea569a63df4c0b5005de2f676e3b
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-03-15 01:18:40 +0530

    ab kal karte hai

--- script.js
+++ script.js
@@ -8,15 +8,6 @@ function sheryAnimation() {
   Shery.hoverWithMediaCircle("#web , #GRAPHIC" /* Element to target.*/, {
     images: ["hover.jpg"] /*OR*/,
   });
-  Shery.textAnimate("#page3 #head" /* Element to target.*/, {
-    //Parameters are optional.
-    style: 1,
-    y: 10,
-    delay: 0.1,
-    duration: 2,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    multiplier: 0.1,
-  });
 }
 sheryAnimation();
 
