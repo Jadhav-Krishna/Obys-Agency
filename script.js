265a36b37ffb405063853d4787baf3a2e5fb0968
Krishna Jadhav <krishnajadhav85017@gmail.com>
2024-03-16 21:40:36 +0530

    bas thode se effect bache hai .....

--- script.js
+++ script.js
@@ -42,11 +42,6 @@ function locomotive() {
       };
     },
 
-    // follwoing line is not required to work pinning on touch screen
-
-    /* pinType: document.querySelector("#main").style.transform
-    ? "transform"
-    : "fixed"*/
   });
 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
@@ -173,3 +168,41 @@ page2ani();
 //   })
 // }
 // page5()
+Shery.imageEffect(".images", {
+  style: 6,
+  debug:true,
+  gooey: true,
+  config: {
+    noiseDetail: { value: 6.11, range: [0, 100] },
+    distortionAmount: { value: 2.9, range: [0, 10] },
+    scale: { value: 59.54, range: [0, 100] },
+    speed: { value: 0.58, range: [0, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.8333333134651184 },
+    ignoreShapeAspect: { value: true },
+    shapePosition: { value: { x: 0, y: 0 } },
+    shapeScale: { value: { x: 0.5, y: 0.5 } },
+    shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+    shapeRadius: { value: 0, range: [0, 2] },
+    currentScroll: { value: 0 },
+    scrollLerp: { value: 0.07 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.27, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 0 },
+    noise_speed: { value: 0.84, range: [0, 10] },
+    metaball: { value: 0.44, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.38, range: [0, 2] },
+    noise_scale: { value: 8.4, range: [0, 100] },
+  },
+});
